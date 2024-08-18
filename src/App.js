05f83b775fc0e69e0ee762b1ff23aa7696ac3320
Jillian FitzMaurice <fitzmauricejillian@gmail.com>
2024-08-18 15:39:51 -0600

    tailwind added and work section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import { Card } from "./components/Card";
 import AItutor from "./assets/aitutor.png";
 import Github from "./assets/github-color.svg";
 import Boltstack from "./assets/boltstack.png";
-import Edqualis from "./assets/edqualis.png"
+import Edqualis from "./assets/edqualis.png";
+import Phone from "./assets/iphonenew.png";
+import Resume from "./assets/resume.png";
+import Mail from "./assets/mailme.png";
+import Bg from "./assets/bg.png";
+import Linkedin from "./assets/linkedin.png";
 
 const App = () => {
   const scrollToTop = () => {
@@ -42,66 +47,96 @@ const App = () => {
 
       {/* Page Sections */}
       <Element name="section1" className="section section1">
-        <h2>howdy</h2>
         <div className="child-section">
           <div className="inner-child-section">
-            <p>
-              Hi! I'm Jillian, a dynamic artist, UI/UX developer, and front-end
-              builder who thrives on pushing creative boundaries. I'm passionate
-              about crafting unique and engaging digital experiences that stand
-              out from the status quo. Let's create something amazing together!
-            </p>
+            <div>
+              <h2>Howdy</h2>
+
+              <p>
+                Hi! I'm Jillian, a dynamic artist, UI/UX developer, and
+                front-end builder who thrives on pushing creative boundaries.
+                I'm passionate about crafting unique and engaging digital
+                experiences that stand out from the status quo. Let's create
+                something amazing together!
+              </p>
+            </div>
             <img className="profile-pic" src={me} alt="My Example" />{" "}
           </div>
 
           <div className="icon-section">
             <img className="icons" src={Github} />
-            <img className="icons" src={Github} />
-            <img className="icons" src={Github} />
-            <img className="icons" src={Github} />
+            <div >
+            <a  href="https://www.linkedin.com/in/jillianfitzmaurice100/"
+                target="_blank"
+              >
+                <img className="icons" src={Linkedin} alt="linkedin logo" />
+              </a>
+            
+            </div>
+            <div>
+              <img className="icons" src={Mail} />{" "}
+              <a href="mailto:fitzmauricejillian@gmail.com">
+                {" "}
+                fitzmauricejillian@gmail.com{" "}
+              </a>
+            </div>
+            <div>
+              <img className="icons" src={Phone} />{" "}
+              <a
+                className="contacta"
+                href="tel:3039013230"
+                onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"
+              >
+                303.901.3230
+              </a>
+            </div>
+            <img className="icons" src={Resume} />
           </div>
         </div>
       </Element>
 
-      <Element name="section2" className="section section2">
-        <h2>Section 2</h2>
+      <Element name="section2" className="section2-parent">
+        <div className="section section2">
+          <div class="w-full">
+            {" "}
+            <h2>Work</h2>
+          </div>
+          <Card
+            imgSrc={legacy}
+            imgAlt="lawyer app"
+            title="Legacy Architects"
+            info="Recent collaborative Figma project for estate planning lawyers. Password is shyfidelity."
+            link="https://www.figma.com/design/ZulEUpRGw8VCuAIIwSchlI/Lawyer-side-of-Legacy-Architects?node-id=2-282&t=m3vfyViyT4aNMUUV-1"
+            buttonText="Go to Figma project"
+          />
 
-        <Card
-          imgSrc={legacy}
-          imgAlt="lawyer app"
-          title="Legacy Architects"
-          info="Recent collaborative Figma project for estate planning lawyers. Password is shyfidelity."
-          link="https://www.figma.com/design/ZulEUpRGw8VCuAIIwSchlI/Lawyer-side-of-Legacy-Architects?node-id=2-282&t=m3vfyViyT4aNMUUV-1"
-          buttonText="Go to Figma project"
-        />
+          <Card
+            imgSrc={AItutor}
+            imgAlt="a cute pictuer of a computer talking about AI"
+            title="AI Tutor"
+            info="Here are Figma screens and a live link of an AI tutor site, my latest project using chat gpt. I created all the designs, brand aesthetic, and the working front end build. The live link is available in the Figma file. Enjoy!"
+            link="https://www.figma.com/design/IFub1w2Bx04gq61DbCnyt5/AI-TUTOR?node-id=37-3088&t=aIfRkgyvpJaP4cUd-1"
+            buttonText="Go to Figma project"
+          />
+          <Card
+            imgSrc={Boltstack}
+            imgAlt="a screenshot of a Boltstack website talking about AI"
+            title="Boltstack"
+            info="Discover Boltstack.com, where I led marketing efforts and front-end development. I created everything on this site from graphics to functionality and CSS media queries for a mobile-friendly design. Built on internal low-code software."
+            link="https://boltstack-dev.softwarebbd.com/"
+            buttonText="Go to site"
+          />
 
-        <Card
-          imgSrc={AItutor}
-          imgAlt="a cute pictuer of a computer talking about AI"
-          title="AI Tutor"
-          info="Here are Figma screens and a live link of an AI tutor site, my latest project using chat gpt. I created all the designs, brand aesthetic, and the working front end build. The live link is available in the Figma file. Enjoy!"
-          link="https://www.figma.com/design/IFub1w2Bx04gq61DbCnyt5/AI-TUTOR?node-id=37-3088&t=aIfRkgyvpJaP4cUd-1"
-          buttonText="Go to Figma project"
-        />
-              <Card imgSrc={Boltstack}
-      imgAlt="a screenshot of a Boltstack website talking about AI"
-       title="Boltstack"
-       info="Discover Boltstack.com, where I led marketing efforts and front-end development. I created everything on this site from graphics to functionality and CSS media queries for a mobile-friendly design. Built on internal low-code software."
-    
-       link= "https://boltstack-dev.softwarebbd.com/"
-       buttonText="Go to site"
-       />
-
-<Card imgSrc={Edqualis}
-      imgAlt="a screenshot of a Boltstack website talking about AI"
-       title="Edqualis"
-       info="Edqualis is an expansive education platform that engages admins, teachers, and students in one place. With the ability to generate content, forms, custom Dashboards, classes, lessons, rubrics, and custom branded UI.
+          <Card
+            imgSrc={Edqualis}
+            imgAlt="a screenshot of a Boltstack website talking about AI"
+            title="Edqualis"
+            info="Edqualis is an expansive education platform that engages admins, teachers, and students in one place. With the ability to generate content, forms, custom Dashboards, classes, lessons, rubrics, and custom branded UI.
        Reskinned the entire application to give a more modern look."
-      link= "https://esp-dev.softwarebbd.com/?rr_dev=true&"
-       buttonText="Log in available upon request"
-       />
-
-
+            link="https://esp-dev.softwarebbd.com/?rr_dev=true&"
+            buttonText="Log in available upon request"
+          />
+        </div>
       </Element>
 
       <Element name="section3" className="section section3">
